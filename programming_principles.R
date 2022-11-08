e0a5525810160815f49fe6b37c5259bbe84152ec
Max Diamond <diamax98@gmail.com>
2022-11-07 19:15:12 -0500

    Learned basics about R dtypes and vars

---
+++ programming_principles.R
@@ -0,0 +1,40 @@
+## DATA TYPES
+# integer
+x <- 2L
+typeof(x)
+
+# double
+y <- 2
+typeof(y)
+z <- 2.5
+typeof(z)
+
+# complex
+a <- 3 + 2i
+typeof(a)
+
+# character
+b <- 'blah blah'
+typeof(b)
+
+# logical
+c <- T
+typeof(c)
+d <- F
+typeof(d)
+
+
+## USING VARIABLES
+a <- 10
+b <- 5
+c <- a + b
+
+var1 <- 2.5
+var2 <- 4
+result = var1/var2
+
+ans <- sqrt(var2)
+
+greeting <- 'hello'
+name <- 'ben'
+msg <- paste(greeting, name)
