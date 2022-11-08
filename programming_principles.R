11ee665116d0b5a413b614384f018d48ee246eae
Max Diamond <diamax98@gmail.com>
2022-11-08 18:33:59 -0500

    Learned about logicals, while, for, and if statements

--- programming_principles.R
+++ programming_principles.R
@@ -38,3 +38,49 @@ ans <- sqrt(var2)
 greeting <- 'hello'
 name <- 'ben'
 msg <- paste(greeting, name)
+
+
+## LOGICAL VARS AND OPERATORS
+4 < 5
+10 > 100
+4 == 5
+# ==, !=, <, >, <=, >=, !, |, &, isTRUE(x)
+
+result <- 4 < 5
+result
+typeof(result)
+
+res2 <- !TRUE
+res2
+
+isTRUE(result)
+
+
+## WHILE LOOP
+counter <- 1
+while(counter < 12){
+  print(counter)  # need print to send to console from inside loop
+  counter <- counter + 1
+}
+
+
+## FOR LOOP
+for(i in 1:5){
+  print(i)
+}
+
+1:5  # This represents a VECTOR in R!
+
+
+## IF STATEMENT
+# Generate random # from normal dist
+rm(ans)  # remove a var
+a <- rnorm(1)
+if(a > 1){
+  ans <- 'greater than 1!'
+} else if(a >= -1){
+    ans <- 'between -1 and 1'
+} else{
+    ans <- 'less than -1'
+}
+
